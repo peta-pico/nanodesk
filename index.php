4a268e1b28157113105a61365acd2390d49b7d95
MitchelAwesome <mitchel@austinmedia.nl>
2015-11-06 16:17:21 +0100

    Upload Dashboard
    Upload of dashboard

---
+++ index.php
@@ -0,0 +1,120 @@
+<!doctype html>
+<html class="no-js" lang="">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>NanoDesk</title>
+    <meta name="description" content="Nanodesk">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <!-- Place favicon.ico in the root directory -->
+
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <link href='https://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
+    <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+</head>
+<body>
+	<!--[if lt IE 8]>
+	    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+	<![endif]-->
+<div class="site-container header-main">
+		<div class="site-wrapper">
+			<header class="">
+				<nav class="nav-main">
+					<div class="header-main__branding">
+						<a href="#"><img src="images/logo.png" alt="NanoDesk"></a>
+					</div>
+				
+					<ul class="nav-main__cta">
+						<li><a href="#">Sign Up</a></li>
+						<li><a href="#">Log In</a></li>
+					</ul>
+				</nav>
+			</header>
+		</div><!--//site wrapper-->
+</div><!--//site container-->
+
+<div class="site-container site-main">
+	<div class="site-wrapper">
+
+
+		<aside class="nav-aside">
+			<nav>
+				<ul>
+					<li>item 1</li>
+					<li>item 2</li>
+					<li>item 3</li>
+					<li>item 4</li>
+					<li>item 1</li>
+					<li>item 2</li>
+					<li>item 3</li>
+					<li>item 4</li>
+					<li>item 5</li>
+					<li>item 6</li>
+					<li>item 7</li>
+					<li>item 8</li>
+					<li>item 9</li>
+					<li>item 10</li>
+					<li>item 11</li>
+					<li>item 12</li>
+					<li>item 13</li>
+					<li>item 14</li>
+					<li>item 15</li>
+					<li>item 16</li>
+					<li>item 17</li>
+					<li>item 18</li>
+					<li>item 19</li>
+					<li>item 20</li>
+				</ul>
+			</nav>
+		</aside>
+
+		<main>
+			<div class="module-block">
+				<div class="module-block__header">
+					Block title
+				</div>
+				<div class="module_block__content">
+					content
+				</div>
+			</div>
+		</main>
+	</div><!--//site wrapper-->
+</div><!--//site container-->
+
+<div class="site-container footer-main">
+	
+		<div class="site-wrapper">
+			<footer class="footer-content">
+
+			<p>
+				&copy; Nanodesk
+			</p>
+			</footer>
+		</div>
+	
+</div>
+
+
+
+	<?php
+    //<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    //<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.3.min.js"><\/script>')</script>
+    ?>
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>
+
+    <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+    <script>
+        (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+        function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+        e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+        e.src='https://www.google-analytics.com/analytics.js';
+        r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+        ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+    </script>
+</body>
+</html>
